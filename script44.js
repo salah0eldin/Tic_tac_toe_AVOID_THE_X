64c92e008d6a8ee664fafbb0ea7b34a66d0e0beb
salah0eldin <67404369+salah0eldin@users.noreply.github.com>
2023-03-25 23:20:29 +0200

    Add files via upload

---
+++ script44.js
@@ -0,0 +1,333 @@
+// get DOM elements
+const gameBoard = document.getElementById("game");
+const cells = document.querySelectorAll(".cell");
+const startGameButton = document.getElementById("start-game");
+const gameTypeRadios = document.getElementsByName("game-type");
+const gameModeRadios = document.getElementsByName("game-mode");
+const gridn = document.getElementsByName("gridn");
+const messageElement = document.getElementById("game-message");
+
+// game variables
+let currentPlayer;
+let currentPlayerC;
+let currentPlayerCC = "Blue";
+let gameType;
+let gameMode;
+let board;
+let togglecount = false;
+let endd = true;
+
+// add event listeners to buttons
+startGameButton.addEventListener("click", startGame);
+
+gameModeRadios.forEach((radioo) => {
+  radioo.addEventListener('click', () => {
+    if (!endd) {
+      gameMode = getSelectedRadioValue(gameModeRadios);
+      if (gameMode === 'single') {
+        currentPlayerCC = currentPlayerC;
+        if (gameType === 'regular')
+          computerMove();
+        else
+          computerMoveR();
+      }
+    }
+  })
+});
+
+//check if grid number is changed after end
+gridn.forEach((gridd) => {
+  gridd.addEventListener('click', () => {
+    if (endd) {
+      gridnum = getSelectedRadioValue(gridn);
+      if (gridnum === "33")
+        window.location.replace("index.html");
+      else
+        window.location.replace("index44.html");
+    }
+  })
+});
+
+// start a new game
+function startGame() {
+  endd = false;
+  gameType = getSelectedRadioValue(gameTypeRadios);
+  gameMode = getSelectedRadioValue(gameModeRadios);
+  board = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""];
+
+  // initialize game variables
+  togglecount = !togglecount;
+
+  if (togglecount % 2) {
+    currentPlayer = "X";
+    currentPlayerC = "Black";
+  } else {
+    if (gameType === "regular") currentPlayer = "O";
+    else currentPlayer = "X";
+    currentPlayerC = "Blue";
+  }
+  currentPlayerCC = "Blue";
+  // clear board and add event listeners to cells
+  cells.forEach((cell) => {
+    cell.textContent = "";
+    cell.style.color = 'black';
+    cell.classList.remove('redblue');
+    cell.classList.remove('redblack');
+    cell.addEventListener("click", handleCellClick);
+  });
+
+  // update game message
+  if (gameType === "regular")
+    updateGameMessage(`Player ${currentPlayer}'s turn`);
+  else updateGameMessage(`${currentPlayerC}'s turn`);
+
+  // if single player mode, let computer make the first move
+  if (gameMode === "single" && currentPlayerC === currentPlayerCC) {
+    if (gameType === 'regular')
+      computerMove();
+    else
+      computerMoveR();
+  }
+}
+
+// play a move
+function playMove(index) {
+  board[index] = currentPlayer;
+  cells[index].textContent = currentPlayer;
+  if (currentPlayerC === "Blue")
+    cells[index].style.color = 'blue';
+
+  // check for winner
+  const winner = checkWinner();
+
+  if (winner) {
+    endGame(winner);
+  } else if (board.indexOf("") === -1) {
+    endGame("tie");
+  } else {
+    // switch players
+    if (gameType === "regular")
+      currentPlayer = currentPlayer === "X" ? "O" : "X";
+    currentPlayerC = currentPlayerC === "Black" ? "Blue" : "Black";
+
+    // if single player mode, let computer make a move
+    if (gameMode === 'single' && currentPlayerC === currentPlayerCC)
+      if (gameType === 'regular')
+        computerMove();
+      else
+        computerMoveR();
+    // update game message
+    if (!endd)
+      if (gameType === "regular")
+        updateGameMessage(`Player ${currentPlayer}'s turn`);
+      else
+        updateGameMessage(`${currentPlayerC}'s turn`);
+  }
+}
+
+// function computer move
+function computerMove() {
+  let bestScore = -Infinity;
+  let move;
+
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] === '') {
+      board[i] = currentPlayer;
+      const score = minimax(board, 0, false, currentPlayer, -Infinity, Infinity);
+      board[i] = '';
+      if (score > bestScore) {
+        bestScore = score;
+        move = i;
+      }
+    }
+  }
+  playMove(move);
+}
+
+function minimax(board, depth, isMaximizingPlayer, playerMark, alpha, beta) {
+  let opponentMark;
+  playerMark === 'X' ? opponentMark = 'O' : opponentMark = 'X';
+  if (checkForWinner(board, playerMark)) {
+    return 11 - depth;
+  } else if (checkForWinner(board, opponentMark)) {
+    return depth - 11;
+  } else if (depth == 9 || !board.includes('')) {
+    return 0;
+  }
+
+  if (isMaximizingPlayer) {
+    let bestScore = -Infinity;
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] === '') {
+        board[i] = playerMark;
+        const score = minimax(board, depth + 1, false, playerMark, alpha, beta);
+        board[i] = '';
+        bestScore = Math.max(bestScore, score);
+        alpha = Math.max(alpha, bestScore);
+        if (beta <= alpha) {
+          break;
+        }
+      }
+    }
+    return bestScore;
+  } else {
+    let bestScore = Infinity;
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] === '') {
+        board[i] = opponentMark;
+        const score = minimax(board, depth + 1, true, playerMark, alpha, beta);
+        board[i] = '';
+        bestScore = Math.min(bestScore, score);
+        beta = Math.min(beta, bestScore);
+        if (beta <= alpha) {
+          break;
+        }
+      }
+    }
+    return bestScore;
+  }
+}
+
+function computerMoveR() {
+  let bestScore = Infinity;
+  let move;
+  let alpha = -Infinity;
+  let beta = Infinity;
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] === '') {
+      board[i] = currentPlayer;
+      let score = minimaxR(board, 0, false, alpha, beta);
+      board[i] = '';
+      if (score < bestScore) {
+        bestScore = score;
+        move = i;
+      }
+    }
+  }
+  playMove(move);
+}
+
+function minimaxR(board, depth, isMinimizingPlayer, alpha, beta) {
+  if (checkForWinner(board, currentPlayer)) {
+    if (!isMinimizingPlayer)
+      return 6 - depth;
+    else
+      return depth - 6;
+  }
+  else if (depth == 5 || !board.includes(''))
+    return 0;
+  if (isMinimizingPlayer) {
+    let bestScore = Infinity;
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] === '') {
+        board[i] = currentPlayer;
+        const score = minimaxR(board, depth + 1, false, alpha, beta);
+        board[i] = '';
+        bestScore = Math.min(bestScore, score);
+        beta = Math.min(beta, bestScore);
+        if (beta <= alpha) {
+          break;
+        }
+      }
+    }
+    return bestScore;
+  } else {
+    let bestScore = -Infinity;
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] === '') {
+        board[i] = currentPlayer;
+        const score = minimaxR(board, depth + 1, true, alpha, beta);
+        board[i] = '';
+        bestScore = Math.max(bestScore, score);
+        alpha = Math.max(alpha, bestScore);
+        if (beta <= alpha) {
+          break;
+        }
+      }
+    }
+    return bestScore;
+  }
+
+}
+
+function checkForWinner(board, mark) {
+  const winningCombos = [    [0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15], // rows
+    [0, 4, 8, 12], [1, 5, 9, 13], [2, 6, 10, 14], [3, 7, 11, 15], // columns
+    [0, 5, 10, 15], [3, 6, 9, 12] // diagonals
+  ];
+
+  return winningCombos.some(combo => combo.every(index => board[index] === mark));
+}
+
+// check if there is a winner
+function checkWinner() {
+  const winningCombinations = [
+    [0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15], // rows
+    [0, 4, 8, 12], [1, 5, 9, 13], [2, 6, 10, 14], [3, 7, 11, 15], // columns
+    [0, 5, 10, 15], [3, 6, 9, 12] // diagonals
+  ];
+
+  let test = false;
+
+  for (let combination of winningCombinations) {
+
+    const [a, b, c, d] = combination;
+
+    if (board[a] && board[a] === board[b] && board[a] === board[c] && board[a] == board[d]) {
+      test = true;
+      if (gameType === "inverse")
+        setTimeout(function () {
+          for (let x of combination) {
+            let cellcolor = window.getComputedStyle(cells[x]).getPropertyValue('color');
+            if (cellcolor === 'rgb(0, 0, 0)')
+              cells[x].classList.add('redblack');
+            else
+              cells[x].classList.add('redblue');
+          }
+        }, 300);
+      else
+        for (let x of combination)
+          cells[x].style.color = 'red';
+    }
+  }
+  return test;
+}
+
+// end the game
+function endGame(winner) {
+  endd = true;
+  // remove event listeners from cells
+  cells.forEach((cell) => {
+    cell.removeEventListener("click", handleCellClick);
+  });
+
+  // update game message
+  if (winner === "tie") {
+    updateGameMessage("It is a tie!");
+  } else {
+    if (gameType === "regular") updateGameMessage(`Player ${currentPlayer} wins!`);
+    else updateGameMessage(`${currentPlayerC} loses!`);
+  }
+}
+
+// get the value of the selected radio button
+function getSelectedRadioValue(radios) {
+  for (let radio of radios) {
+    if (radio.checked) {
+      return radio.value;
+    }
+  }
+}
+
+// update the game message
+function updateGameMessage(message) {
+  messageElement.textContent = message;
+}
+
+//changing content when click
+function handleCellClick() {
+  const index = Array.from(cells).indexOf(this);
+  if (this.textContent === "") {
+    playMove(index);
+  }
+}
